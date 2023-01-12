4dee1dee62aabfe0f2f26c9c036e3dc7197bb345
Luismkm <luismarcelo13@yahoo.com.br>
2023-01-11 21:10:23 -0300

    feat: apply responsive layout in app banner component

--- src/pages/landing/components/appBanner.tsx
+++ src/pages/landing/components/appBanner.tsx
@@ -4,27 +4,27 @@ import googlePlay from '../../../assets/google_play.png';
 
 export function AppBanner() {
   return (
-    <section className="max-w-[1120px] mx-auto flex justify-between items-center pl-36 pt-28 pb-28">
+    <section className="max-w-[1120px] h-[960px] ss:h-[1066px] my-0 sm:my-12 md:my-24 lg:my-0 px-4 mx-auto flex lg:flex-row justify-center items-center">
       <div>
-        <strong className="text-4xl font-black leading-[150%] mb-10 block">
+        <strong className="text-[2rem] ss:text-[38px] sm:text-[2.5rem] md:text-[3rem] leading-[150%] lg:text-[3rem] text-center xl:text-start font-black  mb-10 block">
           DOWNLOAD APP &
-          {' '}
           <br />
           GET THE VOUCHER!
         </strong>
-        <p className="text-lg text-gray-600 mb-14 leading-[32px]">
+        <div className="lg:hidden max-w-[560px] bg-white-100">
+          <img src={mobile} alt="" className="object-contain w-[100%] h-[100%]" />
+        </div>
+        <p className="text-lg text-center xl:text-start text-gray-600 mb-14 leading-[32px]">
           Get 30% off for first transaction using
-          {' '}
           <br />
-          {' '}
           Rondovision mobile app for now.
         </p>
-        <div className="flex flex-row gap-4 h-[54px]">
+        <div className="flex flex-row justify-center xl:justify-start gap-4 h-[54px]">
           <img src={appleStore} alt="" />
           <img src={googlePlay} alt="" />
         </div>
       </div>
-      <div className="max-w-[560px] bg-white-100">
+      <div className="hidden lg:block max-w-[560px] bg-white-100">
         <img src={mobile} alt="" className="object-contain w-[100%] h-[100%]" />
       </div>
     </section>
